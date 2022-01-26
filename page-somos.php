bf808e5751d9cdb8b58bf427d62a91f89326a93f
Gustavo Rivero <tavomak@gmail.com>
2022-01-26 12:32:48 -0300

    Landing pasos v1

--- page-somos.php
+++ page-somos.php
@@ -66,10 +66,10 @@ $bloque_3_imagen = get_field('bloque_3_imagen');
         <div class="col-12 py-5">
             <a href="<?php echo site_url('/pasos-a-seguir'); ?>">
                 <div class="pasos-wrapper py-4 px-md-5 mb-4">
-                    <ul class="d-flex justify-content-between align-items-center">
+                    <ul class="d-md-flex justify-content-between align-items-center">
                         <li>
                             <h3 class="mb-0 text-white mb-xl-2">Pasos a seguir...</h3>
-                            <ul class="d-flex align-items-center">
+                            <ul class="d-md-flex align-items-center">
                                 <li><img src="<?php bloginfo('template_directory');?>/assets/img/tocar.png" alt="play" width="45"></li>
                                 <li class="px-4"><p class="text-white mt-4" style="font-size: 1.2rem;">Te guiamos para que la compra de tu depto sea sencilla e informada</p></li>
                             </ul>
