75237e3f45ecd99fc1ae0ba5b2468a392ee446b5
Gustavo Rivero <tavomak@gmail.com>
2023-01-10 09:25:35 -0300

    USA

---
+++ page-usa.php
@@ -0,0 +1,28 @@
+<?php
+    get_template_part('includes/header'); 
+    bk_main_before();
+/*
+Template Name: Comercial
+*/
+?>
+<?php 
+$taxonomy = 'tipo';
+$query = new WP_Query(array(
+    'post_type'      	=> 'proyectos',
+    'posts_per_page'	=> -1,
+    'post_status'		=> 'publish',
+    'tax_query'         => array (
+        array(
+            'taxonomy'      => $taxonomy,
+            'field'          => 'slug',
+            'terms'         => 'usa'
+        ),
+    ),
+  ));
+  include( locate_template( './includes/templates/proyectos_destacados.php', false, false) ); ?>
+
+
+<?php 
+    bk_main_after();
+    get_template_part('includes/footer'); 
+?>
