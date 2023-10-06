79ed4c7f5db194fac9ed717069235ffdb5f3ada0
Gustavo Rivero <tavomak@gmail.com>
2023-10-06 09:10:12 -0300

    [Development-update] Plantas form

--- single-plantas.php
+++ single-plantas.php
@@ -74,7 +74,7 @@ $correos_ventas = $grupo_de_datos['correos_ventas'];
 
 <?php if (have_posts()) : ?>
 <section id="plantas" class="container-fluid my-5">
-    <div class="row align-items-center">
+    <div class="row">
         <?php while (have_posts()) : the_post();
                 $post_id = get_the_ID();
                 $superficie_total = get_field('superficie_total');
@@ -114,10 +114,10 @@ $correos_ventas = $grupo_de_datos['correos_ventas'];
             </p>
         </div>
 
-        <div class="col-lg-4 mt-5 mt-md-0 mb-5">
+        <div class="col-lg-4 pt-5 mb-5">
             <div class="plantas-info-wrap px-md-5 d-flex flex-column align-items-center">
                 <div class="wrapper w-100">
-                    <h2 class=""><span class="plantas-info-title"><?php echo esc_html($label); ?></span></h2>
+                    <h2 class="pb-5"><span class="plantas-info-title"><?php echo $texto_titulo; ?> <?php echo esc_html($label); ?> + <?php echo $cantidad_de_banos; echo ($cantidad_de_banos == "1") ? " Baño" : " Baños"; ?></span></h2>
                     <?php if($unidades ): ?>
                         <p class="pb-4"><small><?php echo $unidades; ?></small></p>
                     <?php endif;?>
@@ -189,6 +189,7 @@ $correos_ventas = $grupo_de_datos['correos_ventas'];
 
         <div class="col-lg-4 mt-5 pr-md-5">
             <h2 class="d-md-none pb-4"><span class="plantas-info-title">Solicitar Información</span></h2>
+            <h2 class="pb-2"><span class="plantas-info-title">Déjanos tus datos y te contactará un asesor</span></h2>
             <div class="wp-block-contact-form-7-contact-form-selector w-100">
                 <div class="wpcf7 brickcf7 w-100" role="form"  id="wpcf7-f560-o1" lang="es-ES" dir="ltr" class="w-100 wp">
                     <div class="screen-reader-response">
@@ -213,6 +214,7 @@ $correos_ventas = $grupo_de_datos['correos_ventas'];
                             <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f560-o1">
                             <input type="hidden" name="_wpcf7_container_post" value="0">
                             <input type="hidden" name="nombreProyecto" class="nombreProyecto">
+                            <input type="hidden" name="producto" class="inputProducto" value="<?php echo $texto_titulo; ?> <?php echo esc_html($label); ?> + <?php echo $cantidad_de_banos; echo ($cantidad_de_banos == "1") ? " Baño" : " Baños"; ?>">
                             <input type="hidden" name="logoProyecto" class="logoProyecto">
                             <input type="hidden" name="superficieUtil" class="superficieUtil">
                             <input type="hidden" name="superficieTerraza" class="superficieTerraza">
